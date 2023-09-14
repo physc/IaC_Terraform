4ff1b3a12fc18a000e1a8b12300ef38c5b694e7e
physc <34573402+physc@users.noreply.github.com>
2023-09-14 06:17:38 +0000

    new script

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "rgname" {
+  type        = string
+  description = "The name of the resource group"
+}
+
+variable "rglocation" {
+  type        = string
+  description = "The region for the deployment"
+}
