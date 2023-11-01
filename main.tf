84aea909c79963528c8ca9d4f62df498b17ed710
physc <34573402+physc@users.noreply.github.com>
2023-11-01 11:28:33 +0000

    Done

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 terraform {
+
+cloud {
+organization = "Tanush-org"
+workspaces {
+  name = "Azure_DEV"
+}
+ }
+
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
--- main/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "rgname" {
-  type        = string
-  description = "The name of the resource group"
-}
-
-variable "rglocation" {
-  type        = string
-  description = "The region for the deployment"
-}
