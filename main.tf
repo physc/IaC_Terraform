0f17b86adbf9c66d7b41d46eb1fb3f7521a1cae7
physc <34573402+physc@users.noreply.github.com>
2023-11-01 11:28:41 +0000

    Done

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-terraform {
-
-cloud {
-organization = "Tanush-org"
-workspaces {
-  name = "Azure_DEV"
-}
- }
-
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.71.0"
-
-    }
-  }
-}
 provider "azurerm" {
   skip_provider_registration = "true"
   features {}
--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,18 @@
 terraform {
-  backend "azurerm" {
-    key  = "github.tfstate"
+
+cloud {
+organization = "Tanush-org"
+workspaces {
+  name = "Azure_DEV"
+          }
+ }
+
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.71.0"
+
+    }
   }
 }
